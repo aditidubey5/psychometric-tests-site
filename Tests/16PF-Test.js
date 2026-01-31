f1d2802f0ec055818ad79afd5a09173270de2b7e
Aditi Dubey <aditidubey1105@gmail.com>
2026-01-31 22:10:01 +0530

    Update 16PF Test name and description

--- Tests/16PF-Test.js
+++ Tests/16PF-Test.js
@@ -1,6 +1,6 @@
 {
-  name: "Personality Test (External)",
-  description: "16PF Test",
+  name: "16PF Test",
+  description: "Personality Test",
   duration: "10 minutes",
   type: "Self-report",
   link: "https://openpsychometrics.org/tests/16PF.php"
